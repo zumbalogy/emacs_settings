6da7e012cfafcd82256e79b64240f163e400b45f
zumbalogy <intrapostal@gmail.com>
2018-02-08 00:59:45 -0600

    treemacs new version can now clickable and all

--- treemacs.el
+++ treemacs.el
@@ -30,15 +30,6 @@
 
 (setq treemacs-header-function #'treemacs-header)
 
-(require 'color-theme-buffer-local)
-(add-to-list 'custom-theme-load-path "~/emacs/themes")
-
-(add-hook 'treemacs-mode-hook
-          (lambda ()
-            (linum-mode -1)
-            (load-theme-buffer-local 'my-treemacs (get-buffer " *Treemacs-Framebuffer-1*"))
-            (define-key treemacs-mode-map [mouse-1] 'treemacs-visit-node-default-action)))
-
 ;; TODO: have it follow version control of a buffer
 ;; TODO: would be nice to color the background properly
 ;; (set-face-attribute 'hl-line nil :foreground "black" :background "yellow")
@@ -61,3 +52,41 @@
 ;; or less colorful.
 
 ;; TODO: ignore certain file types (like *.elc)
+
+(defun my-treemacs-click-action (event)
+  "Move focus to the clicked line.
+Must be bound to a mouse click, or EVENT will not be supplied."
+  (interactive "e")
+  (when (eq 'mouse-1 (elt event 0))
+    (unless (eq major-mode 'treemacs-mode)
+      ;; no when-let - the window must exist or this function would not be called
+      (select-window (treemacs--is-visible?)))
+    (goto-char (posn-point (cadr event)))
+    (when (region-active-p)
+      (keyboard-quit))
+    ;; 7th element is the clicked image
+    (treemacs-do-for-button-state
+     :on-dir-node-closed  (treemacs--expand-dir-node btn)
+     :on-dir-node-open    (treemacs--collapse-dir-node btn)
+     :on-file-node-closed (treemacs-visit-node-no-split btn)
+     :on-file-node-open   (treemacs-visit-node-no-split btn)
+     :no-error            t)
+    (treemacs--evade-image)))
+;; TODO: get this to move focus to new buffer
+
+;; also, look into dired-sidebar, as its faster
+;; and treemacs is now super slow.
+;; just have to figure out nice sorting
+;; for that one.
+
+
+
+(require 'color-theme-buffer-local)
+(add-to-list 'custom-theme-load-path "~/emacs/themes")
+
+(add-hook 'treemacs-mode-hook
+          (lambda ()
+            (linum-mode -1)
+            (load-theme-buffer-local 'my-treemacs (get-buffer " *Treemacs-Framebuffer-1*"))
+            (define-key treemacs-mode-map [mouse-1] 'my-treemacs-click-action)
+            ))
