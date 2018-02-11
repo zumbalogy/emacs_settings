0b6c891dbf49635dff083ed6d1b2483b6877c909
zumbalogy <intrapostal@gmail.com>
2018-02-11 02:12:48 -0600

    load order changed and now emacsclient background color behavoing better on startup

--- theme.el
+++ theme.el
@@ -1,26 +1,6 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(fringe-mode 0)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (load-theme 'atom-one-dark t)
-
 (require 'doom-themes)
 
-;; Global settings (defaults)
-(setq doom-themes-enable-bold t)   ; if nil, bold is universally disabled
-(setq doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
-;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
-;; may have their own settings.
-(load-theme 'doom-one t)
-
-;; Enable flashing mode-line on errors
-;; (doom-themes-visual-bell-config)
-
-;; Corrects (and improves) org-mode's native fontification.
-;; (doom-themes-org-config)
+(fringe-mode 0)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -53,3 +33,18 @@
 (set-face-background 'mode-line-inactive "#1c1e24")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t)   ; if nil, bold is universally disabled
+(setq doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; may have their own settings.
+(load-theme 'doom-one t)
+;; (load-theme 'atom-one-dark t)
+
+;; Enable flashing mode-line on errors
+;; (doom-themes-visual-bell-config)
+
+;; Corrects (and improves) org-mode's native fontification.
+;; (doom-themes-org-config)
